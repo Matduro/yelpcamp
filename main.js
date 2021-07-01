7c18ddcb25825f11c0b430c5054615a37d1ef56d
Mathieu <mathieurousseau123@gmail.com>
2021-06-30 22:38:14 -0700

    create main file

---
+++ main.js
@@ -0,0 +1,7 @@
+const lorem  = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore distinctio eum ipsa? Deserunt fugiat porro corrupti iste aspernatur. Aliquam minus, quis consequatur quia ad veniam eveniet exercitationem alias dignissimos aut?";
+
+const maps = [1, 2, 3];
+
+const newMaps = maps.map(x => x * 2);
+
+console.log(newMaps);
