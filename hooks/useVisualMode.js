2862f84ee0384c75a972a075df3536a3e1a74cc9
Mathieu <mathieurousseau123@gmail.com>
2021-07-03 13:13:24 -0700

    clean hooks code

--- hooks/useVisualMode.js
+++ hooks/useVisualMode.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
-  // const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
@@ -10,38 +9,14 @@ export default function useVisualMode(initial) {
       setHistory(newHistory);
     }
     setHistory((prev) => [...prev, newMode]);
-    // setMode(newMode);
   }
 
   function back() {
     if (history.length > 1) {
       const newHistory = history.slice(0, -1);
       setHistory(newHistory);
-      //setMode(newHistory[newHistory.length - 1]);
     }
   }
 
   return { mode: history[history.length - 1], transition, back };
 }
-
-//
-////
-//////
-////////
-// The below options work, but are not recommended as they use .pop() which mutates arrays, and we don't want that.
-///////////////////////
-
-// function transition(newMode, replace = false) {
-//   if (replace) {
-//     history.pop();
-//   }
-//   setHistory((prev) => [...prev, newMode]);
-//   setMode(newMode);
-// }
-// function back() {
-//   if (history.length > 1) {
-//     history.pop();
-//     setHistory(history);
-//     setMode(history[history.length - 1]);
-//   }
-// }
