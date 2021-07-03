bd727a19eb5a77fb1160d02da1a78d35212d31d2
Mathieu <mathieurousseau123@gmail.com>
2021-07-03 13:17:47 -0700

    change data in main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const lorem  = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore distinctio eum ipsa? Deserunt fugiat porro corrupti iste aspernatur. Aliquam minus, quis consequatur quia ad veniam eveniet exercitationem alias dignissimos aut?";
 
-const maps = [1, 2, 3];
+const maps = [1, 2, 3, 4, 5, 6];
 
 const newMaps = maps.map(x => x * 2);
 
