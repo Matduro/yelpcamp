9c54caf364635723d95bd7ec593dd52e69e36615
Mathieu <mathieurousseau123@gmail.com>
2021-07-07 22:40:36 -0700

    Collection complete

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const lorem  = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore distinctio eum ipsa? Deserunt fugiat porro corrupti iste aspernatur. Aliquam minus, quis consequatur quia ad veniam eveniet exercitationem alias dignissimos aut?";
 
-const maps = [1, 2, 3, 4, 5, 6, 7];
+const maps = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const newMaps = maps.map(x => x * 2);
 
