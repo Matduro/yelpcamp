1b43feae9d85ca925d7d8567e6b2165709d0acb5
Mathieu <mathieurousseau123@gmail.com>
2021-07-08 21:45:34 -0700

    update notes and dependencies

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const lorem  = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore distinctio eum ipsa? Deserunt fugiat porro corrupti iste aspernatur. Aliquam minus, quis consequatur quia ad veniam eveniet exercitationem alias dignissimos aut?";
+const lorem =
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore distinctio eum ipsa? Deserunt fugiat porro corrupti iste aspernatur. Aliquam minus, quis consequatur quia ad veniam eveniet exercitationem alias dignissimos aut?";
 
 const maps = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const newMaps = maps.map(x => x * 2);
+const newMaps = maps.map((x) => x * 2);
 
 console.log(newMaps);
 console.log(lorem);
+
+// React project
