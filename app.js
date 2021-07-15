b468a9285bfc6e569376206f611612117c3f1bef
Mathieu <mathieurousseau123@gmail.com>
2021-07-15 13:58:00 -0700

    set up campground page and connection on app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
