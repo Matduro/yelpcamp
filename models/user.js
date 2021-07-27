125c36fe5eaa9927ab885458a151195c46c7f9cd
Mathieu <mathieurousseau123@gmail.com>
2021-07-27 16:46:01 -0700

    update middleware.js with authorization functions

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require("passport-local-mongoose");
 
+// Passport will add a field for username and a password, and will ensure that username input is unique. There is also additional methods.
 const UserSchema = new Schema({
   email: {
     type: String,
