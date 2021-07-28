a15d4873d8818bf0fbc2841e560cb1b52d1c2e5b
Mathieu <mathieurousseau123@gmail.com>
2021-07-28 11:47:47 -0700

    Refactor code, remove security issues. Add css

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,6 @@
 const User = require("../models/user");
 
 module.exports.renderRegister = (req, res) => {
-  if (req.isAuthenticated()) {
-    return res.redirect("/campgrounds");
-  }
   res.render("users/register");
 };
 
@@ -24,20 +21,19 @@ module.exports.register = async (req, res, next) => {
 };
 
 module.exports.renderLogin = (req, res) => {
-  if (req.isAuthenticated()) {
-    return res.redirect("/campgrounds");
-  }
   res.render("users/login");
 };
 
 module.exports.login = (req, res) => {
-  req.flash("success", "Welcome back!");
+  req.flash("success", "welcome back!");
   const redirectUrl = req.session.returnTo || "/campgrounds";
+  delete req.session.returnTo;
   res.redirect(redirectUrl);
 };
 
 module.exports.logout = (req, res) => {
   req.logout();
+  // req.session.destroy();
   req.flash("success", "Goodbye!");
   res.redirect("/campgrounds");
 };
